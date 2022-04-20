9b4a1d08ff96bc88cf7d9bc33f3ee135e81a136c
Tomas Balciunas <tamasius@gmail.com>
2022-04-20 20:51:51 +0300

    sudeti kableliai

--- wedish/wedish_site/models.py
+++ wedish/wedish_site/models.py
@@ -15,47 +15,46 @@ class Category(MP_Node):
 
 
 data = [{'data':{'name':'Category'}, 'children':[
-            {'data':{'name':'Product'}}, 'children':[
-                {'data':{'name':'Meal'}}, 'children':[
+            {'data':{'name':'Product'}, 'children':[
+                {'data':{'name':'Meal'}, 'children':[
                     {'data':{'name':'Breakfast'}},
                     {'data':{'name':'Brunch'}},
                     {'data':{'name':'Lunch'}},
                     {'data':{'name':'Hi-Tea'}},
                     {'data':{'name':'Dinner'}},
                     {'data':{'name':'Supper'}},
-                    ],
-                {'data':{'name':'Drink'}}, 'children':[
+                    ]},
+                {'data':{'name':'Drink'}, 'children':[
                     {'data':{'name':'Soft drinks'}},
                     {'data':{'name':'Coctail'}},
                     {'data':{'name':'Alcohol'}},
                     {'data':{'name':'Alcohol coktail'}},
                     {'data':{'name':'Tea'}},
                     {'data':{'name':'Coffee'}},
-                    ],
-                {'data':{'name':'Other'}}, ]
-            {'data':{'name':'Service'}}, 'children':[
-                    {'data':{'name':'Place to eat'}},
-                    {'data':{'name':'Take out'}},
-                    ],
-            {'data':{'name':'General_Product'}}, 'children':[
-                    {'data':{'name':'Grains'}},
-                    {'data':{'name':'Vegetable'}},
-                    {'data':{'name':'Fruits'}},
-                    {'data':{'name':'Meat'}},
-                    {'data':{'name':'Liquids'}},
-                    {'data':{'name':'Spices'}}
-                    {'data':{'name':'Eggs, milk...'}}
-                    ],
+                    ]},
+                {'data':{'name':'Other'}},
+                ]},
+            {'data':{'name':'Service'}, 'children':[
+                {'data':{'name':'Place to eat'}},
+                {'data':{'name':'Take out'}},
+                ]},
+            {'data':{'name':'General_Product'}, 'children':[
+                {'data':{'name':'Grains'}},
+                {'data':{'name':'Vegetable'}},
+                {'data':{'name':'Fruits'}},
+                {'data':{'name':'Meat'}},
+                {'data':{'name':'Liquids'}},
+                {'data':{'name':'Spices'}},
+                {'data':{'name':'Eggs, milk...'}},
+                 ]},
             {'data':{'name':'Brand'}},
             {'data':{'name':'Unit'},  'children':[
-                    {'data':{'name':'Weight'}},
-                    {'data':{'name':'Temperature'}},
-                    {'data':{'name':'Data'}},
-                    {'data':{'name':'Time'}},
-                    {'data':{'name':'Lenght'}},
-                    {'data':{'name':'Currency'}},
-                    ], 
-                ]},
+                {'data':{'name':'Weight'}},
+                {'data':{'name':'Temperature'}},
+                {'data':{'name':'Data'}},
+                {'data':{'name':'Time'}},
+                {'data':{'name':'Lenght'}},
+                {'data':{'name':'Currency'}},
+                ]}, 
+            ]},
 ]
-
-MyNodeModel.load_bulk(data, None)
